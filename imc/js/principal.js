4e618ecf9bc211ab85d30c618a23c201218cd4a4
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-03-31 21:36:01 -0300

    update

--- imc/js/principal.js
+++ imc/js/principal.js
@@ -1,7 +1,8 @@
 var paciente = document.querySelector('#primeiro-paciente');
 var tdPeso = paciente.querySelector('.inf-peso');
 var tdAltura = paciente.querySelector('.inf-altura');
+var tdImc = paciente.querySelector('.inf-imc');
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
 var imc = peso / (altura * altura);
-console.log(imc);
+tdImc.textContent = imc;
