5b564010a5392ac03109db767a27795e863a7943
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-03-31 21:15:16 -0300

    update

--- imc/js/principal.js
+++ imc/js/principal.js
@@ -1,3 +1,7 @@
 // IMC = peso / altura x altura
 var paciente = document.querySelector('#primeiro-paciente');
+var tdPeso = paciente.querySelector('.inf-peso');
+var peso = tdPeso.textContent;
 console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
