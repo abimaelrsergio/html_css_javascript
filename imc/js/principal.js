5787a0e322c7813fc2f86f4c8a533ee711da5412
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-04-07 20:16:38 -0300

    update

--- imc/js/principal.js
+++ imc/js/principal.js
@@ -4,5 +4,19 @@ var tdAltura = paciente.querySelector('.inf-altura');
 var tdImc = paciente.querySelector('.inf-imc');
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
+var pesoEhValido = true;
+var alturaEhValida = true;
+if (peso <= 0 || peso >= 1000){
+    console.log('Peso inválido');
+    pesoEhValido = false;
+    tdImc.textContent = 'Peso inválido';
+}
+if (altura <= 0 || altura >= 3.0) {
+    console.log('Altura inválida');
+    alturaEhValida = false;
+    tdImc.textContent = 'Altura inválida';
+}
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
