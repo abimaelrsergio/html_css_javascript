0d8eb8f7bf5a62a0da1b0c6406c60f3d09b93987
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-04-07 21:39:35 -0300

    update

--- imc/js/principal.js
+++ imc/js/principal.js
@@ -12,13 +12,13 @@ for(var i = 0; i < pacientes.length; i++){
         console.log('Peso inválido');
         pesoEhValido = false;
         tdImc.textContent = 'Peso inválido';
-        paciente.style.backgroundColor = 'lightcoral';
+        paciente.classList.add('cliente-invalido');
     }
     if (altura <= 0 || altura >= 3.0) {
         console.log('Altura inválida');
         alturaEhValida = false;
         tdImc.textContent = 'Altura inválida';
-        paciente.style.backgroundColor = 'lightcoral';
+        paciente.classList.add('cliente-invalido');
     }
     if (alturaEhValida && pesoEhValido) {
         var imc = peso / (altura * altura);
