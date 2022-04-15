a369a8f023fd043ced729b3cfddfab098ab49179
abimaelrsergio <abimaelr.sergio@gmail.com>
2022-04-14 21:31:40 -0300

    update

--- imc/js/principal.js
+++ imc/js/principal.js
@@ -22,16 +22,31 @@ for(var i = 0; i < pacientes.length; i++){
         var imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2);
     }
-    var botao = document.querySelector('#salvar-cliente');
-    botao.addEventListener('click', (event) => { 
-        event.preventDefault();
-        
-        var form = document.querySelector('#form-adicionar');
-        var nome = form.nome.value;
-        var peso = form.peso.value;
-        var altura = form.altura.value;
-        var gordura = form.gordura.value;
-
-        console.log(nome, peso, altura, gordura);
-    } );
 }
+
+var botao = document.querySelector('#salvar-cliente');
+botao.addEventListener('click', (event) => { 
+    event.preventDefault();
+    
+    var form = document.querySelector('#form-adicionar');
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var clienteTr = document.createElement('tr');
+    var nomeTd = document.createElement('td');
+    var pesoTd = document.createElement('td');
+    var alturaTd = document.createElement('td');
+    var gorduraTd = document.createElement('td');
+    var imcTd = document.createElement('td');
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    console.log(clienteTr);
+    console.log(nomeTd, pesoTd, alturaTd, gorduraTd);
+} );
+
