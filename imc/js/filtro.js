7d108bf2ab2694016b8d77b137e1e426e475bb08
Abimael Sergio <abimaelsergio@rivian.com>
2022-05-19 21:25:39 -0300

    update

--- imc/js/filtro.js
+++ imc/js/filtro.js
@@ -1,9 +1,22 @@
 var campoFiltro = document.querySelector('#filtrar-tabela');
 campoFiltro.addEventListener('input', function() {
-    console.log('Digitaram no campo', this.value);
     var clientes = document.querySelectorAll('.paciente');
     for (var i = 0; i < clientes.length; i++) {
         var cliente = clientes[i];
+        var tdNome = cliente.querySelector('.info-nome');
+        var nome = tdNome.textContent;
+        if (this.value.length > 0) {
+            if (nome != this.value) {
+                cliente.classList.add('invisivel');
+            } else {
+                cliente.classList.remove('invisivel');
+            }
+        } else {
+            for (var i = 0; i < clientes.length; i++) {
+                var cliente = clientes[i];
+                cliente.classList.remove('invisivel');
+            }
+        }
     }
 });
 
